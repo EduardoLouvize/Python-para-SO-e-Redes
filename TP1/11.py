b0d7a483309764f56398fed6f0cccf4bc479d838
Eduardo Louvize <eduardolouvize@gmail.com>
2021-02-14 07:43:01 -0300

    Initial commit

--- TP1/11.py
+++ TP1/11.py
@@ -2,8 +2,8 @@
 # de bloco de notas (notepad) para abri-lo.
 import os
 
-# nome_arquivo = input("Digite o nome do arquivo: ")
-nome_arquivo = "arquivo_teste.txt"
+nome_arquivo = input("Digite o nome do arquivo: ")
 
 executavel_com_path = os.environ['SYSTEMROOT'] + '\\System32\\notepad.exe'
-os.spawnv(os.P_WAIT, executavel_com_path, [nome_arquivo])
+os.spawnv(os.P_WAIT, executavel_com_path, [executavel_com_path, nome_arquivo])
+
